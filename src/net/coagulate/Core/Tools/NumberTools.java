9749723f004af0b1d65076e27eb09c09ce167711
Iain Price (LUNA) <git@predestined.net>
2018-12-03 01:45:07 +0000

    fix :P

--- src/net/coagulate/Core/Tools/NumberTools.java
+++ src/net/coagulate/Core/Tools/NumberTools.java
@@ -7,11 +7,14 @@ package net.coagulate.Core.Tools;
 public class NumberTools {
 
     public static String fixdp(float number, int dp) {
+        System.out.println("SRC "+number);
         int whole=(int) Math.round(Math.floor(number));
-        int decimal=Math.round((number-whole)*(10^dp));
+        int decimal=Math.round((number-whole)*((float)(10^dp)));
         String decstr=decimal+"";
         while (decstr.length()<dp) { decstr=decstr+"0"; }
-        return whole+"."+decstr;
+        String out=whole+"."+decstr;
+        System.out.println("OUT "+out);
+        return out;
     }
     
 }
