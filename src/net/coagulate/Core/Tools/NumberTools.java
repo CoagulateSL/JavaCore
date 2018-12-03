c57894ad3723ef7a9f0815a2aa448e659ddc4255
Iain Price (LUNA) <git@predestined.net>
2018-12-03 01:50:02 +0000

    optionalise debug

--- src/net/coagulate/Core/Tools/NumberTools.java
+++ src/net/coagulate/Core/Tools/NumberTools.java
@@ -5,19 +5,19 @@ package net.coagulate.Core.Tools;
  * @author Iain Price
  */
 public class NumberTools {
-
+    private static final boolean debug=true;
     public static String fixdp(float number, int dp) {
-        System.out.println("SRC "+number);
+        if (debug) { System.out.println("SRC "+number); }
         int whole=(int) Math.round(Math.floor(number));
-        System.out.println("whole:"+whole);
+        if (debug) { System.out.println("whole:"+whole); }
         int decimal=Math.round((number-whole)*((float)(10^dp)));
-        System.out.println("dec:"+decimal);
+        if (debug) { System.out.println("dec:"+decimal); }
         String decstr=decimal+"";
-        System.out.println("decstr:"+decstr);
+        if (debug) { System.out.println("decstr:"+decstr); }
         while (decstr.length()<dp) { decstr=decstr+"0"; }
-        System.out.println("decstr:"+decstr);
+        if (debug) { System.out.println("decstr:"+decstr); }
         String out=whole+"."+decstr;
-        System.out.println("OUT "+out);
+        if (debug) { System.out.println("OUT "+out); }
         return out;
     }
     
