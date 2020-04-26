49cafe3476fdb4e683aaaa3d39fb75ede62a5460
Iain Price (LUNA) <git@predestined.net>
2020-04-26 01:36:10 +0100

    New Exception

---
+++ src/net/coagulate/Core/Exceptions/User/UserInputValidationFilterException.java
@@ -0,0 +1,18 @@
+package net.coagulate.Core.Exceptions.User;
+
+/**
+ * User input doesn't parse correctly
+ */
+
+public class UserInputValidationFilterException extends UserInputValidationException {
+	private static final long serialVersionUID=1L;
+
+	public UserInputValidationFilterException(final String reason) {
+		super(reason);
+	}
+
+	public UserInputValidationFilterException(final String reason,
+	                                          final Throwable cause) {
+		super(reason,cause);
+	}
+}
