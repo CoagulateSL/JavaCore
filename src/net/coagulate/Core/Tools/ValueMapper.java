ea480dae87f335eb6122dfde642479068577610b
Iain Price (LUNA) <git@predestined.net>
2020-07-22 16:11:16 +0100

    Simple code to map various strings to boolean
    Possibly redundant, The JDK api probably has something like this :P

---
+++ src/net/coagulate/Core/Tools/ValueMapper.java
@@ -0,0 +1,16 @@
+package net.coagulate.Core.Tools;
+
+import net.coagulate.Core.Exceptions.System.SystemBadValueException;
+import net.coagulate.Core.Exceptions.User.UserInputValidationParseException;
+
+import javax.annotation.Nonnull;
+
+public class ValueMapper {
+
+    public static boolean toBoolean(@Nonnull String value) {
+        value=value.toLowerCase();
+        if (value.isEmpty() || value.equals("0") || value.equals("false") || value.equals("no") || value.equals("n") || value.equals("off")) { return false; }
+        if (value.equals("1") || value.equals("true") || value.equals("yes") || value.equals("y") || value.equals("on")) { return false; }
+        throw new UserInputValidationParseException("Value "+value+" does not reduce to boolean");
+    }
+}
