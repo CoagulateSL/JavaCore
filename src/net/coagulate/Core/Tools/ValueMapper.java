64f6f90924fa4e3de41fdd0adfa3080f5c3fd4c4
Iain Price (LUNA) <git@predestined.net>
2020-07-23 05:27:00 +0100

    ahem

--- src/net/coagulate/Core/Tools/ValueMapper.java
+++ src/net/coagulate/Core/Tools/ValueMapper.java
@@ -10,7 +10,7 @@ public class ValueMapper {
     public static boolean toBoolean(@Nonnull String value) {
         value=value.toLowerCase();
         if (value.isEmpty() || value.equals("0") || value.equals("false") || value.equals("no") || value.equals("n") || value.equals("off")) { return false; }
-        if (value.equals("1") || value.equals("true") || value.equals("yes") || value.equals("y") || value.equals("on")) { return false; }
+        if (value.equals("1") || value.equals("true") || value.equals("yes") || value.equals("y") || value.equals("on")) { return true; }
         throw new UserInputValidationParseException("Value "+value+" does not reduce to boolean");
     }
 }
