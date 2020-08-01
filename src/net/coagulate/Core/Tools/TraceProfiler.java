8b07b034ba4295e03a43957f17d10eef1ae80e8e
Iain Price (LUNA) <git@predestined.net>
2020-08-01 01:35:41 +0100

    Basic stacktrace recorder

---
+++ src/net/coagulate/Core/Tools/TraceProfiler.java
@@ -0,0 +1,40 @@
+package net.coagulate.Core.Tools;
+
+import net.coagulate.Core.Exceptions.System.SystemExecutionException;
+
+import java.util.*;
+
+public class TraceProfiler {
+
+    private static final Map<String, Map<String,Integer>> profiles=new HashMap<>(); // map of profile name to map of stack trace to count
+
+    public static void profile(String setname) {
+        Throwable t=new SystemExecutionException("Profile tracer");
+        String signature=ExceptionTools.toString(t);
+        if (!profiles.containsKey(setname)) { profiles.put(setname,new HashMap<>()); }
+        Map<String,Integer> profile=profiles.get(setname);
+        if (!profile.containsKey(signature)) { profile.put(signature,1); return; }
+        profile.replace(signature,profile.get(signature)+1);
+    }
+
+    public static Set<String> profiles() { return profiles.keySet(); }
+
+    public static String reportProfile(String setname) {
+        // seems this class could technically be instantiable but i apparently didn't write it that way
+        Map<String,Integer> profile=new HashMap<>(profiles.get(setname)); // avoid concurrent modification exception :P hopefully
+        StringBuilder report=new StringBuilder();
+        report.append("<table border=1><tr><th>Count</th><th>Trace</th></tr>");
+        Set<Integer> counts=new TreeSet<>(Comparator.reverseOrder());
+        counts.addAll(profile.values());
+        for (Integer value:counts) {
+            for (String trace:profile.keySet()) {
+                if (profile.get(trace).equals(value)) {
+                    report.append("<tr><td>").append(value).append("</td><td><pre>").append(trace).append("</pre></td></tr>");
+                }
+            }
+        }
+        report.append("</table>");
+        return report.toString();
+    }
+
+}
