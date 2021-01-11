2ecca96827c935bea3c29fa2ea46f2942210bef4
Iain Price (LUNA) <git@predestined.net>
2021-01-11 09:23:49 +0000

    Allow suppression

--- src/net/coagulate/Core/Exceptions/User/UserInputNotFoundException.java
+++ src/net/coagulate/Core/Exceptions/User/UserInputNotFoundException.java
@@ -7,6 +7,14 @@ package net.coagulate.Core.Exceptions.User;
 public class UserInputNotFoundException extends UserInputValidationException {
 	private static final long serialVersionUID=1L;
 
+	public UserInputNotFoundException(String message, Throwable exception, boolean suppress) {
+		super(message, exception, suppress);
+	}
+
+	public UserInputNotFoundException(String reason, boolean suppress) {
+		super(reason, suppress);
+	}
+
 	public UserInputNotFoundException(final String reason) {
 		super(reason);
 	}
