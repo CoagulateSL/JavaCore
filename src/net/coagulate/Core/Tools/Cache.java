ff26a0a4d8f545cae94129c678fb50a67f7bf19e
Iain Price (LUNA) <git@predestined.net>
2021-01-25 18:58:29 +0000

    Change Cache to use functional interfaces

--- src/net/coagulate/Core/Tools/Cache.java
+++ src/net/coagulate/Core/Tools/Cache.java
@@ -3,38 +3,42 @@ package net.coagulate.Core.Tools;
 import javax.annotation.Nonnull;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static net.coagulate.Core.Tools.UnixTime.getUnixTime;
 
 public class Cache <T> {
 
     private final static Map<String,Cache<?>> caches=new ConcurrentHashMap<>();
+    private final int expiration;
 
-    public static <S> Cache<S> getCache(@Nonnull String name) {
-        name=name.toLowerCase();
-        if (caches.containsKey(name)) {
-            //noinspection unchecked
-            return (Cache<S>)caches.get(name);
-        }
-        return makeCache(name);
+    public Cache(int expiration) {
+        this.expiration=expiration;
     }
 
-    private static synchronized <S> Cache<S> makeCache(@Nonnull String name) {
+    public static <S> Cache<S> getCache(@Nonnull String name,int expiration) {
         name=name.toLowerCase();
         if (caches.containsKey(name)) {
             //noinspection unchecked
             return (Cache<S>)caches.get(name);
         }
-        Cache<S> cache=new Cache<>();
+        return makeCache(name,expiration);
+    }
+
+    private static synchronized <S> Cache<S> makeCache(@Nonnull String name,int expiration) {
+        name=name.toLowerCase();
+        if (caches.containsKey(name)) {
+            //noinspection unchecked
+            return (Cache<S>)caches.get(name);
+        }
+        Cache<S> cache=new Cache<>(expiration);
         caches.put(name,cache);
         return cache;
     }
 
     public static void maintenance() {
-        Collection<Cache<?>> cacheset=caches.values();
-        for (Cache<?> cache:cacheset) {
+        Collection<Cache<?>> cacheSet=caches.values();
+        for (Cache<?> cache:cacheSet) {
             try { cache.maintenanceThis(); }
             catch (ConcurrentModificationException e) {
                 Logger.getLogger("CacheMaintenance").log(Level.FINE,"Cache maintenance died",e);
@@ -44,51 +48,37 @@ public class Cache <T> {
 
     private void maintenanceThis() {
         int now=UnixTime.getUnixTime();
-        Set<String> deleteset=new HashSet<>();
+        Set<String> deleteSet=new HashSet<>();
         for (Map.Entry<String,CacheElement<T>> row:cache.entrySet()) {
-            if (now>row.getValue().expires) { deleteset.add(row.getKey()); }
+            if (now>row.getValue().expires) { deleteSet.add(row.getKey()); }
         }
-        for (String row:deleteset) { cache.remove(row); }
+        for (String row:deleteSet) { cache.remove(row); }
     }
 
     private final Map<String,CacheElement<T>> cache=new ConcurrentHashMap<>();
+
+    private int cacheHit=0;
+    private int cacheMiss=0;
 
     /**
      * Get an object from the cache
      *
      * @param key Cache key
+     * @param supplier Functional Supplier for the value, if not cached
      *
      * @return Object from the cache
-     *
-     * @throws CacheMiss If there is no cached object by that key
      */
-    public T get(@Nonnull final String key) throws CacheMiss {
-        if (!cache.containsKey(key)) { throw new CacheMiss(); }
-        final CacheElement<T> ele=cache.get(key);
-        if (ele==null) { throw new CacheMiss(); }
-        if (ele.expires<getUnixTime()) {
-            cache.remove(key);
-            throw new CacheMiss();
+    public T get(@Nonnull final String key, Supplier<T> supplier) {
+        int now=UnixTime.getUnixTime();
+        if (cache.containsKey(key)) {
+            if (cache.get(key).expires<now) { cache.remove(key); }
         }
-        return ele.element;
-    }
-
-    /**
-     * Store an element in the cache
-     *
-     * @param key             Cache key
-     * @param object          Cache element
-     * @param lifetimeseconds How long to cache for in seconds
-     *
-     * @return The object being cached (object)
-     */
-    @Nonnull
-    public T put(@Nonnull final String key,
-                 @Nonnull final T object,
-                 final int lifetimeseconds) {
-        final CacheElement<T> ele=new CacheElement<>(object,getUnixTime()+lifetimeseconds);
-        cache.put(key,ele);
-        return object;
+        CacheElement<T> cached=cache.getOrDefault(key,null);
+        if (cached==null) {
+            cached=new CacheElement<>(supplier.get(),UnixTime.getUnixTime()+expiration);
+            cacheMiss++;
+        } else { cacheHit++; }
+        return cached.element;
     }
 
     public void purge(String name) {
@@ -107,9 +97,5 @@ public class Cache <T> {
             this.element=element;
             this.expires=expires;
         }
-    }
-
-    public static class CacheMiss extends Exception {
-        private static final long serialVersionUID=1L;
     }
 }
