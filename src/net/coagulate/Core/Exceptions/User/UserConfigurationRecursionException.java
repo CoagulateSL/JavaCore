be362f2557bba1eda846237e70dfc530763c63fe
Iain Price (LUNA) <git@predestined.net>
2022-04-17 23:02:36 +0100

    Add serial version to serialisable classes.  Then don't use it properly.

--- src/net/coagulate/Core/Exceptions/User/UserConfigurationRecursionException.java
+++ src/net/coagulate/Core/Exceptions/User/UserConfigurationRecursionException.java
@@ -1,6 +1,8 @@
 package net.coagulate.Core.Exceptions.User;
 
 public class UserConfigurationRecursionException extends UserConfigurationException {
+
+    private static final long serialVersionUID = 1L;
     public UserConfigurationRecursionException(String reason) {
         super(reason);
     }
