be0cbced3ec70ad644bb60293a8140275bcdc2f4
Iain Price (LUNA) <git@predestined.net>
2023-03-19 03:41:01 +0000

    Do not reset the list of all caches. Client classes will still reference those caches so all we do is lose visibility of them. Instead clear the cache content.

--- src/net/coagulate/Core/Tools/Cache.java
+++ src/net/coagulate/Core/Tools/Cache.java
@@ -14,9 +14,9 @@ public class Cache<U,T> {
 	 * If set to true forces all cache elements to refresh every couple of seconds, regardless of their preference.
 	 * For use during node transitions.
 	 */
-	public static  boolean                eagerCacheFlush=false;
-	private static Map<String,Cache<?,?>> caches         =new ConcurrentHashMap<>();
-	private final  int                    expiration;
+	public static        boolean                eagerCacheFlush=false;
+	private static final Map<String,Cache<?,?>> caches         =new ConcurrentHashMap<>();
+	private final        int                    expiration;
 	private final Map<U,CacheElement<T>> cache=new ConcurrentHashMap<>();
 	private long cacheHit;
 	private long cacheMiss;
@@ -122,7 +122,15 @@ public class Cache<U,T> {
 	 */
 	public static void enableCache() {
 		eagerCacheFlush=false;
-		caches=new ConcurrentHashMap<>(); // merry garbage collection day?
+		// oh god why, dont do this
+		// caches=new ConcurrentHashMap<>(); // merry garbage collection day?
+		// the using classes have a static reference, you MUST purge all the content instead
+		synchronized(caches) {
+			for (final Cache cache:
+			     caches.values()) {
+				cache.purgeAll();
+			}
+		}
 	}
 	
 	/**
