0bdb7b401ce550d552b8a045f19578bf116e9815
Iain Price (mondas) <git@predestined.net>
2026-09-17 15:04:30 +0100

    Can hide a column too (allowing for hidden sort)

--- src/net/coagulate/Core/HTML/Elements/Table.java
+++ src/net/coagulate/Core/HTML/Elements/Table.java
@@ -39,32 +39,49 @@ public class Table extends TagPair {
 	}
 
 	public String toString() {
-		if (sortOn>-1) {
-			contents().sort((a,b)->{
-				if (a instanceof TableRow) {
-					for (Container c:a.contents()) {
-						if (c instanceof TableHeader) { return -1; }
+		if (!doneHide) {
+			if (sortOn > -1) {
+				contents().sort((a, b) -> {
+					if (a instanceof TableRow) {
+						for (Container c : a.contents()) {
+							if (c instanceof TableHeader) {
+								return -1;
+							}
+						}
 					}
-				}
-				if (b instanceof TableRow) {
-					for (Container c:b.contents()) {
-						if (c instanceof TableHeader) { return 1; }
+					if (b instanceof TableRow) {
+						for (Container c : b.contents()) {
+							if (c instanceof TableHeader) {
+								return 1;
+							}
+						}
 					}
-				}
-				if (a instanceof TableRow && b instanceof TableRow) {
-					if (sortType==SORT_TYPE.DOUBLE) {
-						Double aa=Double.parseDouble(((TableRow)a).get(sortOn).contents().get(0).toString());
-						Double bb=Double.parseDouble(((TableRow)b).get(sortOn).contents().get(0).toString());
-						return (sortReverse?-1:1)*aa.compareTo(bb);
+					if (a instanceof TableRow && b instanceof TableRow) {
+						if (sortType == SORT_TYPE.DOUBLE) {
+							Double aa = Double.parseDouble(((TableRow) a).get(sortOn).contents().get(0).toString());
+							Double bb = Double.parseDouble(((TableRow) b).get(sortOn).contents().get(0).toString());
+							return (sortReverse ? -1 : 1) * aa.compareTo(bb);
+						}
+						return (sortReverse ? -1 : 1) * (((TableRow) a).get(sortOn)).toString().compareTo(((TableRow) b).get(sortOn).toString());
 					}
-					return (sortReverse?-1:1)*(((TableRow)a).get(sortOn)).toString().compareTo(((TableRow)b).get(sortOn).toString());
+					return 0;
+				});
+			}
+			if (hideColumn > -1) {
+				for (Container content:contents()) {
+					if (content instanceof TableRow) { ((TableRow)content).contents().remove(hideColumn); }
 				}
-				return 0;
-			});
+			}
 		}
 		return super.toString();
 	}
 
+	public void hideColumn(int i) {
+		hideColumn=i;
+	}
+
+	private int hideColumn=-1;
+	private boolean doneHide=false;
 	public enum SORT_TYPE {STRING,DOUBLE};
 	private SORT_TYPE sortType=SORT_TYPE.STRING;
 	private int sortOn=-1;
