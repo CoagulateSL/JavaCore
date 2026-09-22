7682e0caf17c8a096ec9ae67bad7702e53a2d89d
Iain Price (mondas) <git@predestined.net>
2026-09-22 01:10:16 +0100

    Linebreak tag

---
+++ src/net/coagulate/Core/HTML/Elements/LineBreak.java
@@ -0,0 +1,13 @@
+package net.coagulate.Core.HTML.Elements;
+
+import net.coagulate.Core.HTML.TagSingle;
+
+import javax.annotation.Nonnull;
+
+public class LineBreak extends TagSingle {
+
+	@Override
+	public String tag() {
+		return "br";
+	}
+}
